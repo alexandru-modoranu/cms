e457f80e223fd6e1a733176aba86ec1946049e7b
Pavel Ionut Catalin <picoplast@yahoo.com>
2015-02-23 13:08:56 +0200

    Update per.h

--- sw/sw_mcu/inc/per.h
+++ sw/sw_mcu/inc/per.h
@@ -10,13 +10,11 @@
 
 /* ST peripherals */
 #define USB_BASE 0x40005C00
-#define USBMEM_BASE 0x40006000
 #define	RCC_BASE 0x40021000
 
 #include "per_usb.h"
 #include "per_rcc.h"
 
-#define USBMEM ((volatile USBMEM_t *)USBMEM_BASE)
 #define USB ((volatile USB_t *)USB_BASE)
 #define RCC ((volatile RCC_t *)RCC_BASE)
 
